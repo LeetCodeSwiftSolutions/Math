10cb5e0d9180f1b9ed711de4500f2622be6b6103
Miguel Torres <99153283+MrPicudo@users.noreply.github.com>
2025-11-16 09:52:23 -0600

    Sqrt of x
    - Simplemente aplicamos lógica para devolver el entero más cercano a la raíz cuadrada de un nùmero (redondeado hacia abajo), sin usar funciones de potencia o de raíz cuadrada integradas.

---
+++ SqrtOfX.playground/Contents.swift
@@ -0,0 +1,21 @@
+/* RAÍZ CUADRADA ENTERA
+Dado un entero no negativo x, devuelve la raíz cuadrada entera de x, es decir, (redondeada hacia abajo). El resultado también debe ser un entero no negativo. No se pueden usar funciones u operadores integrados de potencia/raíz.
+
+Given a non-negative integer x, return the integer square root of x, (rounded down). The result must also be a non-negative integer. You must not use any built-in exponent function or operator. */
+
+
+/// Solución original
+func mySqrt(_ x: Int) -> Int {
+    // Vamos a iterar sobre todos los números multiplicados por sí mismos
+    var pot = 1
+    // Nos detenemos cuando el cuadrado de un número sea mayor a x
+    while pot*pot < x {
+        pot += 1
+    }
+    // Analizamos si el número era la raíz exacta o se pasó.
+    return pot*pot > x ? pot - 1 : pot
+}
+
+
+/// Pruebas
+mySqrt(1414213562)
